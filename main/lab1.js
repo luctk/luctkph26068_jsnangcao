8eac275c1a851f0234e35f9aeee8fa464c7752da
luctk <luctkph26068@fpt.edu.vn>
2023-01-11 14:12:14 +0700

    update

--- main/lab1.js
+++ main/lab1.js
@@ -1,4 +1,35 @@
-players1=['gk1','fieldPlayers1'];
-players2=['gk2','fieldPlayers1'];
-allPlayers=['gk1','gk2','fieldPlayers1','fieldPlayers1'];
-players1Final=['gk1','fieldPlayers1','Thiago','Coutinho','Perisic'];
+//1
+const player1 = []; //team1
+const player2 = []; //team2
+
+//2
+//team1
+const GK1 = "Neuer";
+const fieldPlayer1 = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
+player1.push(GK1, ...fieldPlayer1);
+console.log("player1: ", player1);
+
+//team2
+const GK2 = "Ter Stergen";
+const fieldPlayer2 = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
+player2.push(GK2, ...fieldPlayer2);
+console.log("player2: ", player2);
+
+//3
+const allPlayer = [];
+allPlayer.push(...player1, ...player2);
+console.log("allPlayer: ", allPlayer);
+
+//4
+const player1Final = player1;
+player1Final.splice(8, 3);
+player1Final.push("thiago", "coutinho", "perisic");
+console.log("player1Final thay người", player1Final);
+
+//5
+const team1 = "";
+const team2 = "";
+const draw = "";
+
+//6
+const printGoals = () => {};
